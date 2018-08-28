2241c16001d49a8ab279d9527f5f469b84145ded
Mearat Hou <mearatjames@gmail.com>
2018-08-28 15:44:52 -0700

    Fix long search phrase bug

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,3 @@
-//Global Variables
-
 
 //GiphyAPI Query
 function giphy (searchString, rating) {
@@ -8,23 +6,27 @@ $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response.data);
     for (let i = 0; i < 10; i++) {
         $('#gifCard').append(`
         <div class="card col-sm-6 col-md-4 col-lg-3 mt-2" style="width: 18rem;">
-            <img id="giphy" class="card-img-top" src="${response.data[i].images.original.url}" alt="Card image cap">
+            <img class="giphy card-img-top" data-index="${i}" src="${response.data[i].images.original_still.url}" alt="Card image cap">
             <div class="card-body">
                 <p class="card-text">Rating: ${response.data[i].rating}</p>
             </div>
         </div>
         `)
     }
+    $('.giphy').on('click', function() {
+        let index = $(this).attr("data-index")
+        if ($(this).attr("src") == response.data[index].images.original_still.url) {
+            $(this).attr("src", response.data[index].images.original.url)
+        } else if ($(this).attr("src") == response.data[index].images.original.url) {
+            $(this).attr("src", response.data[index].images.original_still.url)
+        }
+    })
 
   });
 }
-
-
-
 
 //Document Ready
 $(function() {
@@ -34,27 +36,34 @@ $(function() {
 $('#searchAdd').on('click', function() {
     event.preventDefault();
     $('#btn').append(`
-    <button type="button" class="gif col-sm-2 btn btn-secondary mr-1 mt-1">${$('#search').val()}<span class="closebtn">&times;</span></button>
+    <div class="gif col-sm-2 btn btn-secondary mr-1 mt-1">
+    <div class="row align-items-center">
+    <div class="searchvalue col-8">${$('#search').val()}</div>
+    <div class="col-2 closebtn">&times;</div>
+    </div>
     `)
     $('#search').val("")
-    console.log("Submit!")
-    btnClick()
+    btnClick();
+    closeBtn();
 })
 
 //Button Click
 function btnClick() {
-    $('button.gif').on('click', function() {
-        console.log($(this).text())
+    $('.searchvalue').on('click', function() {
         $('#gifCard').empty();
         let searchString = $(this).text();
         let rating = "g";
         giphy(searchString, rating)
-        $('.closebtn').on('click', function() {
-            $(this).parent().remove()
-        })
     })
 }
 btnClick();
 
 //Close Button
+function closeBtn() {
+    $('.closebtn').on('click', function() {
+    $(this).parentsUntil($('#btn')).remove()
+})
+}
+closeBtn()
+
 })
